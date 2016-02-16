b9384da023f93e945df0460f8c09a4174e3bee9a
Kyle Hersey <kyle@Kyles-MacBook-Air.local>
2016-02-15 20:17:45 -0500

    removing angel of death

--- W15_CIS*2750 "Angel of Death" /test/sample/IllegalFieldValueException.java
+++
@@ -1,9 +0,0 @@
-public class IllegalFieldValueException extends Exception {
-	public IllegalFieldValueException(String message) {
-		super(message);
-	}
-	public IllegalFieldValueException(String message, Throwable throwable) {
-		super(message, throwable);
-	}
-}
-
